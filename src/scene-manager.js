5d51167a95b3529f177b4c6c0c6ed1286bd683b9
oskarrough <oskar@rough.dk>
2023-12-08 14:36:52 +0100

    Nicer animations

--- src/scene-manager.js
+++ src/scene-manager.js
@@ -6,7 +6,7 @@ export class Scene extends Node {}
 
 export class SceneManager extends Node {
 	Scenes = QueryAll(Scene)
-	// ['intro', 's<F4>ingle', 'multi']
+	// ['intro', 'single', 'multi']
 	currentScene = Reactive()
 
 	update(changed) {
@@ -15,7 +15,7 @@ export class SceneManager extends Node {
 		if (scene === 'intro') render(el, IntroScene()) && IntroScene.animation()
 		if (scene === 'single') render(el, SinglePlayerScene()) && fadein()
 		if (scene === 'multi') render(el, MultiplayerScene()) && fadein()
-		if (scene === 'quit') render(el, ExitScene()) && ExitScene.animation()
+		if (scene === 'exit') render(el, ExitScene()) && ExitScene.animation()
 	}
 }
 
@@ -38,35 +38,26 @@ function IntroScene() {
 			<slag-menu>
 				<menu>
 					<button type="button" onclick=${() => (manager.currentScene = 'single')}>Single Player †</button>
-					<button type="button" onclick=${() => (manager.currentScene = 'multi')}>Multiplayer ††</button>
+					<button type="button" onclick=${() => (manager.currentScene = 'multi')}>
+					Multiplayer (<small><live-presence /> live</small>)
+					</button>
 					<button
 						type="button"
-						onclick=${() => (manager.currentScene = 'quit')}
+						onclick=${() => (manager.currentScene = 'exit')}
 						data-beepover="29"
 						data-beep="28"
 					>
-						Quit ╳
+						╳
 					</button>
 				</menu>
 			</slag-menu>
-			<div class="Background"></div>
 		</slay-intro-scene>
 	`
 }
-IntroScene.animation = () => {
-	return gsap
-		.timeline()
-		.set('body', {opacity: 1})
-		.from('.Background', {autoAlpha: 0, scale: 1.05, duration: 1.5})
-		.from('h1', {autoAlpha: 0, y: -260, duration: 0.8, rotate: '-3deg', ease: 'power2.out'}, '-=1')
-		.from('menu', {autoAlpha: 0, y: -20, duration: 0.6, ease: 'power2.out'}, '-=0.6')
-		.from('menu > *', {stagger: 0.05, y: -5, autoAlpha: 0, duration: 0.25, ease: 'power2.out'}, '-=0.6')
-}
-
 function SinglePlayerScene() {
 	return html`
 		<slay-scene>
-			<button type="button" onclick=${() => (manager.currentScene = 'intro')}>Exit</button>
+			<menu tr><button type="button" onclick=${() => (manager.currentScene = 'intro')}>↺</button></menu>
 			<slag-mark>
 				<live-lobby autocreate></live-lobby>
 			</slag-mark>
@@ -80,7 +71,7 @@ function SinglePlayerScene() {
 function MultiplayerScene() {
 	return html`
 		<slay-scene>
-			<button type="button" onclick=${() => (manager.currentScene = 'intro')}>Exit</button>
+			<menu tr><button type="button" onclick=${() => (manager.currentScene = 'intro')}>↺</button></menu>
 			<slag-mark>
 				<live-lobby autocreate></live-lobby>
 			</slag-mark>
@@ -90,26 +81,30 @@ function MultiplayerScene() {
 
 function ExitScene() {
 	return html`
-		<slay-intro-scene>
+		<slay-exit-scene>
 			<h1>Slagmark 🌐</h1>
 			<menu>
-				<button type="button" onclick=${() => (manager.currentScene = 'single')}>Single Player †</button>
-				<button type="button" onclick=${() => (manager.currentScene = 'multi')}>Multiplayer ††</button>
-				<button type="button" onclick=${() => (manager.currentScene = 'quit')} data-beep="28">Quit ╳</button>
+				<button type="button" onclick=${() => (manager.currentScene = 'intro')}>↺</button>
 			</menu>
-			<div class="Background"></div>
-		</slay-intro-scene>
+		</slay-exit-scene>
 	`
 }
-ExitScene.animation = () => {
-	const menu = document.querySelector('menu')
-	const title = document.querySelector('h1')
-	const background = document.querySelector('.Background')
+
+IntroScene.animation = () => {
 	return gsap
 		.timeline()
-		.to(menu, {scale: 0, autoAlpha: 0, y: '-50%', duration: 1})
-		.to(title, {y: '-10%', autoAlpha: 0, duration: 1, ease: 'power2.out'}, '<')
-		.to(background, {scale: 1.2, filter: 'brightness(1)', duration: 60}, '<')
+		.set('body', {opacity: 1})
+		.to('.Background', {autoAlpha: 1, scale: 1, duration: 1.5})
+		.from('h1', {autoAlpha: 0, y: '-10%', duration: 0.8, ease: 'power2.out'}, '-=1.4')
+		.from('menu', {autoAlpha: 0, y: -20, duration: 0.6, ease: 'power2.out'}, '-=1.3')
+		.from('menu > *', {stagger: 0.05, y: -5, autoAlpha: 0, duration: 0.25, ease: 'power2.out'}, '-=1.2')
+}
+
+ExitScene.animation = () => {
+	return gsap
+		.timeline()
+		.to('h1', {y: '-10%', autoAlpha: 0, duration: 1, ease: 'power2.out'}, '<')
+		.to('.Background', {autoAlpha: 0.2, scale: 1.2, duration: 1}, '<')
 }
 
 export const manager = SceneManager.new()
