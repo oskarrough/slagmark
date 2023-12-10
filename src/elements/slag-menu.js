4bd358cf675260bc1e1eb62528f0a5631b31e120
oskarrough <oskar@rough.dk>
2023-12-10 18:15:29 +0100

    Smarter bleeps

--- src/elements/slag-menu.js
+++ src/elements/slag-menu.js
@@ -2,17 +2,22 @@ import {beep} from '../stdlib/audio.js'
 
 export class SlagMenu extends HTMLElement {
 	connectedCallback() {
-		this.addEventListener('mouseover', (event) => {
-			const target = event.target.closest('a, button')
-			if (!target) return
-			const x = `bleep-${target.dataset.beepover || 26}.wav`
+		this.enableBleepSounds()
+	}
+
+	enableBleepSounds() {
+		function mouseenter(event) {
+			const x = `bleep-${event.target.dataset.beepover || 26}.wav`
 			beep(x)
-		})
-		this.addEventListener('click', (event) => {
-			const target = event.target.closest('a, button')
-			if (!target) return
-			const x = `bleep-${target.dataset.beep || 30}.wav`
+		}
+		function click(event) {
+			const x = `bleep-${event.target.dataset.beep || 30}.wav`
 			beep(x)
+		}
+		const items = this.querySelectorAll('a, button')
+		items.forEach((item) => {
+			item.addEventListener('mouseenter', mouseenter)
+			item.addEventListener('click', click)
 		})
 	}
 
